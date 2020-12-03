448d56fb518ce890fb3dfef4555405ede88e8059
Olivier <oliviervandeneede@hotmail.com>
2020-12-03 19:52:26 +0100

    Day 3.1

---
+++ day3/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common/utils.h"
+
+#define MAX_BUFFER_SIZE 265
+
+#ifndef INPUT_FILE
+#warning Please define INPUT_FILE on the command line
+#endif
+
+int read_input_file(const char* filename, char*** map, int* width)
+{
+    FILE* file;
+    int counter = 0;
+    char buffer[MAX_BUFFER_SIZE] = "";
+
+    if((file = fopen(filename, "r")) == 0) {
+        perror("Could not open input file\n");
+        exit(1);
+    }
+
+    do {
+        fscanf(file, "%s\n", buffer);
+
+        *width = strnlen(buffer, MAX_BUFFER_SIZE);
+
+        // Get some memory and store the password
+        *map = (char**) realloc(*map, (counter+1) * sizeof(char*));
+        (*map)[counter] = (char*) calloc(*width, sizeof(char));
+        strncpy((*map)[counter], buffer, *width);
+
+        counter++;
+    } while(!feof(file));
+
+    fclose(file);
+    return counter;
+}
+
+int check_slope(char** map, int height, int width, int offset_x, int offset_y)
+{
+    int y=0, x=0;
+    int trees = 0;
+
+    while(y < height) {
+        if (map[y][x % width] == '#') {
+            trees++;
+        }
+
+        x += offset_x;
+        y += offset_y;
+    }
+
+    printf("trees: %d\n", trees);
+
+    return trees;
+}
+
+
+int main(void)
+{
+    char** map = NULL;
+    int length = 0;
+    int width = 0;
+
+    length = read_input_file(INPUT_FILE, &map, &width);
+    printf("Len: %d, Width: %d\n", length, width);
+
+    // Part 1
+    int trees = check_slope(map, length, width, 3, 1);
+    printf("Part 1: %d\n", trees);
+
+    // Part 2
+    int result = 1;
+    result *= check_slope(map, length, width, 1, 1);
+    result *= check_slope(map, length, width, 3, 1);
+    result *= check_slope(map, length, width, 5, 1);
+    result *= check_slope(map, length, width, 7, 1);
+    result *= check_slope(map, length, width, 1, 2);
+
+    printf("Part 2: %d\n", result);
+
+    return 0;
+}
